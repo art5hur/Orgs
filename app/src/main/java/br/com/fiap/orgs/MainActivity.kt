60c17d87114f603ef264825a1d7c6e990ef88584
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-27 21:00:31 -0300

    ClasseProduto

--- app/src/main/java/br/com/fiap/orgs/MainActivity.kt
+++ app/src/main/java/br/com/fiap/orgs/MainActivity.kt
@@ -1,31 +1,40 @@
-package br.com.fiap.orgs
+package carreiras.com.github.orgs.ui.activity
 
+import android.app.Activity
 import android.os.Bundle
-import android.widget.TextView
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import br.com.fiap.orgs.ui.theme.OrgsTheme
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import carreiras.com.github.orgs.R
+import carreiras.com.github.orgs.model.Produto
+import carreiras.com.github.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
+import java.math.BigDecimal
 
-class MainActivity : ComponentActivity() {
+
+class MainActivity : Activity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         // Binding: processo de vinculação do código fonte com o arquivo de layout.
-        val nome = findViewById<TextView>(R.id.nome)
-        nome.text = "Cesta de frutas"
+//        val nome = findViewById<TextView>(R.id.nome)
+//        nome.setText("Cesta de frutas") ou
+//        nome.text = "Cesta de frutas"
+//        val descricao = findViewById<TextView>(R.id.descricao)
+//        descricao.text = "Laranja, manga e uva"
+//        val valor = findViewById<TextView>(R.id.valor)
+//        valor.text = "19.99"
 
-        val descricao = findViewById<TextView>(R.id.descricao)
-        descricao.text = "Laranja e uva"
-
-        val valor = findViewById<TextView>(R.id.valor)
-        valor.text = "R$19.00"
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.adapter = ListaProdutosAdapter(
+            context = this,
+            produtos = listOf(
+                Produto("Teste 0", "Teste Descrição 0", BigDecimal("19.90")),
+                Produto("Teste 1", "Teste Descrição 1", BigDecimal("29.90")),
+                Produto("Teste 2", "Teste Descrição 2", BigDecimal("39.90")),
+            )
+        )
+        // Pode ser configurado no layout
+//        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
-
